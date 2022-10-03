11526dd404813e73d425742496580aefe1dd9514
Kerem Dincer <b.keremdincer@gmail.com>
2022-10-03 20:43:37 +0200

    cleanup

--- src/app.tsx
+++ src/app.tsx
@@ -1,32 +1,19 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import './app.css'
+import { useState } from "preact/hooks";
+import { AddNoteForm } from "./components/AddNoteForm";
+import { NoteEditor } from "./components/NoteEditor";
+import { NoteList } from "./components/NoteList";
 
 export function App() {
-  const [count, setCount] = useState(0)
+  const [selected, setSelected] = useState<number | undefined>();
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
+      <AddNoteForm />
+      <NoteList
+        selected={selected}
+        onSelect={(id: number) => setSelected(id)}
+      />
+      <NoteEditor id={selected} onClose={() => setSelected(undefined)} />
     </>
-  )
+  );
 }
